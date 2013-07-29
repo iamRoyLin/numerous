7efdf37d95a9ffeb1f19bb927a0023fd13493038
Roy Lin <roy@luna.co.nz>
2013-07-29 20:56:59 +1200

    fixed testing

--- www/modules/Music.js
+++ www/modules/Music.js
@@ -8,9 +8,11 @@ var Music = new function () {
 	// call this function to play a sound
 	this.play = function (src) {
 		if (Env.phoneGap) {
+			alert("this is phonegap");
 			myMedia = new Media(src, function() {}, function() {});
 			myMedia.play();
 		} else {
+			alert("this is no phonegap);
 			soundManager.createSound({
 				url: src,
 				autoLoad: true,
