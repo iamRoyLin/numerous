e9ff7ee0663635f9ff7a7e6f824a10d37d7a9d55
Vianna Bao <viannabao@gmail.com>
2013-07-29 20:58:25 +1200

    for unsave changes

--- www/core/views/MenuView.js
+++ www/core/views/MenuView.js
@@ -1,96 +1,49 @@
-var LoginView = {};
+var MenuView = {};
 
 // Image Sources
-LoginView.sources = {};
-LoginView.sources.background = "images/widgets/home_screen.png";
-LoginView.sources.playButton = "images/widgets/play_button.png";
-LoginView.sources.optionsButton = "images/widgets/options_button.png";
+MenuView.sources = {};
+MenuView.sources.numerousTitle = "images/widgets/numerous_title.png";
+MenuView.sources.objective1Title = "images/widgets/objective1_menu_title.png";
 
 // Images
-LoginView.images = {};
+MenuView.images = {};
 
 //	Initialize login view
-LoginView.initialize = function(store) {
-	LoginView.store = store;
-	View.render("LoginView");
-	
-	//$('body').on('keyup', '.search-key', LoginView.findByName)
-	//LoginView.registerEvents();
-	//Controller.routeAnchor(".login_linkToGroupingGame", "GroupingGameView", null);
+MenuView.initialize = function() {
+	View.render("MenuView");
 	
 	// Setup the stage
-	LoginView.stage = new Kinetic.Stage({
+	MenuView.stage = new Kinetic.Stage({
 		container: "container",
 		width: window.innerWidth,
 		height: window.innerHeight
 	});
 	
 	// The main layer (might be the only layer we need)
-	LoginView.backgroundLayer = new Kinetic.Layer();
-	LoginView.stage.add(LoginView.backgroundLayer);	
+	MenuView.backgroundLayer = new Kinetic.Layer();
+	MenuView.stage.add(MenuView.backgroundLayer);	
 	
 	var loader = new PxLoader();
-	LoginView.images.background = loader.addImage(LoginView.sources.background);
-	LoginView.images.playButton = loader.addImage(LoginView.sources.playButton);
-	LoginView.images.optionsButton = loader.addImage(LoginView.sources.optionsButton);
+	MenuView.images.numerousTitle = loader.addImage(MenuView.sources.numerousTitle);
+	MenuView.images.objectiveTitle = loader.addImage(MenuView.sources.objective1Title);
 	
 	// Registers loaded() function, which gets called when images loaded into memory
-	loader.addCompletionListener(LoginView.loaded);
+	loader.addCompletionListener(MenuView.loaded);
 	
 	// Starts loading all the images into memory
 	loader.start();
 }
 
-LoginView.loaded = function () {
+MenuView.loaded = function () {
 	// Call helper functionsthe to draw components
-	LoginView.drawBackground();
-	LoginView.drawButtons();
 	
 	// redraw all widgets
-	LoginView.stage.draw();
+	MenuView.stage.draw();
 }
 
-LoginView.drawBackground = function() {
-	var background = new Kinetic.Image({image: LoginView.images.background});
-	WidgetUtil.glue(background, {
-		glueTop: false,
-		glueLeft: false,
-		width: 1,
-		height: 1,
-		dx: 0,
-		dy: 0
-	});
-	LoginView.backgroundLayer.add(background);
-}
+MenuView.drawButtons = function() {
 
-LoginView.drawButtons = function() {
 
-	//play button
-	var	playButton = new Kinetic.Image({image: LoginView.images.playButton});
-	WidgetUtil.glue(playButton, {
-		glueTop: true,
-		glueLeft: true,
-		width: 0.20,
-		height: 0.28,
-		dx: 0.23,
-		dy: 0.45
-	});
-	LoginView.backgroundLayer.add(playButton);
-	playButton.on('click tap', function () {
-			GroupingGameView.initialize();
-	});
-	
-	//options button
-	var	optionsButton = new Kinetic.Image({image: LoginView.images.optionsButton});
-	WidgetUtil.glue(optionsButton, {
-		glueTop: true,
-		glueLeft: true,
-		width: 0.20,
-		height: 0.28,
-		dx: 0.55,
-		dy: 0.45
-	});
-	LoginView.backgroundLayer.add(optionsButton);
 }
 
 	
