755c329c2e14516e91a3d991aba1e27883d67868
Roy <roy@luna.co.nz>
2013-08-31 11:43:40 +1200

    add test cases for MathUtil and AdditionGameHelper

---
+++ www/tests/testAdditionGameHelper.js
@@ -0,0 +1,15 @@
+
+test("test correct generation of the answer of a question", function() {
+	for(var i = 0; i < 17; i++) {
+		var out = AdditionGameHelper.generateQuestion(i);
+		equal(out.answer, out.left + out.right);
+	}
+});
+
+
+
+
+
+
+
+
